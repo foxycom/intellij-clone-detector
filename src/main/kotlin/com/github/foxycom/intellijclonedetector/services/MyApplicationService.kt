5830dc925cbd090e7a3040c663037f45d0fc5c4e
GitHub Action <action@github.com>
2021-01-30 21:59:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/foxycom/intellijclonedetector/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.foxycom.intellijclonedetector.services
+
+import com.github.foxycom.intellijclonedetector.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/foxycom/intellijclonedetector/services/MyProjectService.kt
+++ src/main/kotlin/com/github/foxycom/intellijclonedetector/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.foxycom.intellijclonedetector.services
 
+import com.github.foxycom.intellijclonedetector.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
